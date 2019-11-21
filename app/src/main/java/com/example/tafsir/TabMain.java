840694fe5dde17fb31d2124f1059e47dacc36a07
dida pradana <didapradana01@gmail.com>
2019-11-21 22:54:55 +0700

    add tab for wajiz and tahili tafseer

--- app/src/main/java/com/example/tafsir/TabMain.java
+++ app/src/main/java/com/example/tafsir/TabMain.java
@@ -8,14 +8,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
 
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.Toast;
 
@@ -24,41 +20,60 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
+public class TabMain extends AppCompatActivity {
     DataHelper dbHelper;
-    Cursor cursor, cursor2;
-    RecyclerView recyclerView;
-    LinearLayoutManager linearLayoutManager;
-    TabHost tab;
+    Cursor cursor;
+    RecyclerView recyclerViewTafsirTahili, recyclerViewTafsirWajiz;
+    LinearLayoutManager linearLayoutManagerTafsirTahili, linearLayoutManagerTafsirWajiz;
+    int currentSura = 1;
 
     ArrayList<DataTafsirModel> dataTafsir = new ArrayList<>();
-    int currentSura = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tab_main);
+
         dbHelper = new DataHelper(getApplicationContext());
 
-        linearLayoutManager = new LinearLayoutManager(
+        linearLayoutManagerTafsirTahili = new LinearLayoutManager(
                 this,
                 LinearLayoutManager.HORIZONTAL,
                 false
         );
 
-        tampil();
+        linearLayoutManagerTafsirWajiz = new LinearLayoutManager(
+                this,
+                LinearLayoutManager.HORIZONTAL,
+                false
+        );
+
+        tabInit();
+        tampilTafsirWajiz();
+        tampilTafsirTahili();
+        getItemPosition();
     }
-    public void tampil(){
-        DataTafsirModel dt,dt2;
+
+    private void tabInit(){
+        TabHost tabs = findViewById(R.id.tabhost);
+        tabs.setup();
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+        spec.setContent(R.id.tab_tafsir_tahili);
+        spec.setIndicator("TAHILI");
+        tabs.addTab(spec);
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab_tafsir_wajiz);
+        spec.setIndicator("WAJIZ");
+        tabs.addTab(spec);
+    }
+
+    public void tampilTafsirTahili(){
+        DataTafsirModel dt;
         TafsirAdapter tafsirAdapter;
         SnapHelper snapHelper = new PagerSnapHelper();
 
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         cursor = db.rawQuery("SELECT sura, aya, juz, text FROM ringkas_kemenag", null);
-        cursor2 = db.rawQuery("SELECT text FROM tahlili GROUP BY juz", null);
-        Log.d("info", "Masook sini!");
-        Log.d("info Cursor", String.valueOf(cursor.getCount()));
         if(cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
                 dt = new DataTafsirModel();
@@ -74,21 +89,52 @@ public class MainActivity extends AppCompatActivity {
             int count = insertFromFile(this, R.raw.ringkas_kemenag);
             int count2 = insertFromFile(this, R.raw.tahlili);
 
-            Log.d("Inserted Data", String.valueOf(count));
-            Log.d("Inserted Data", String.valueOf(count2));
-
-            tampil();
+            tampilTafsirTahili();
         }
 
         Log.d("dataTafsir data", dataTafsir.get(0).getText());
         tafsirAdapter = new TafsirAdapter(this, R.layout.text_item, dataTafsir);
 
-        recyclerView = findViewById(R.id.rvTafsir);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(tafsirAdapter);
-        snapHelper.attachToRecyclerView(recyclerView);
-        getItemPosition();
+        recyclerViewTafsirTahili = findViewById(R.id.rvTafsirTahili);
+        recyclerViewTafsirTahili.setLayoutManager(linearLayoutManagerTafsirTahili);
+        recyclerViewTafsirTahili.setAdapter(tafsirAdapter);
+        snapHelper.attachToRecyclerView(recyclerViewTafsirTahili);
     }
+
+    public void tampilTafsirWajiz(){
+        DataTafsirModel dt;
+        TafsirAdapter tafsirAdapter;
+        SnapHelper snapHelper = new PagerSnapHelper();
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT sura, aya, juz, text FROM ringkas_kemenag", null);
+        if(cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                dt = new DataTafsirModel();
+                dt.setSura(cursor.getInt(0));
+                dt.setAya(cursor.getInt(1));
+                dt.setJuz(cursor.getInt(2));
+                dt.setText(cursor.getString(3));
+                dataTafsir.add(dt);
+            }
+
+        } else {
+            insertFromFile(this, R.raw.trans);
+            int count = insertFromFile(this, R.raw.ringkas_kemenag);
+            int count2 = insertFromFile(this, R.raw.tahlili);
+
+            tampilTafsirWajiz();
+        }
+
+        Log.d("dataTafsir data", dataTafsir.get(0).getText());
+        tafsirAdapter = new TafsirAdapter(this, R.layout.text_item, dataTafsir);
+
+        recyclerViewTafsirWajiz = findViewById(R.id.rvTafsirWajiz);
+        recyclerViewTafsirWajiz.setLayoutManager(linearLayoutManagerTafsirWajiz);
+        recyclerViewTafsirWajiz.setAdapter(tafsirAdapter);
+        snapHelper.attachToRecyclerView(recyclerViewTafsirWajiz);
+    }
+
 
     public int insertFromFile(Context context, int ResourceId) {
         // Reseting Counter
@@ -117,14 +163,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getItemPosition(){
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        recyclerViewTafsirTahili.addOnScrollListener(new RecyclerView.OnScrollListener() {
             int position;
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
 
                 if(newState == RecyclerView.SCROLL_STATE_IDLE){
-//                    Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
                     onSuraChanged(dataTafsir.get(position).getSura());
                 }
             }
@@ -132,7 +177,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                position = linearLayoutManager.findFirstVisibleItemPosition();
+                position = linearLayoutManagerTafsirTahili.findFirstVisibleItemPosition();
+            }
+        });
+        recyclerViewTafsirWajiz.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            int position;
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                if(newState == RecyclerView.SCROLL_STATE_IDLE){
+                    onSuraChanged(dataTafsir.get(position).getSura());
+                }
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                position = linearLayoutManagerTafsirWajiz.findFirstVisibleItemPosition();
             }
         });
     }
@@ -144,4 +206,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
